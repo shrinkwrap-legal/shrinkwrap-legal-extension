d6af8936bbca3aa3c38f86c41932ffba90928cb9
Bernhard <1572917+j3ernhard@users.noreply.github.com>
2025-09-12 20:02:05 +0200

    editorconfig

--- src/Sidepanel.tsx
+++ src/Sidepanel.tsx
@@ -1,73 +1,87 @@
-import "./styles/shrinkwrap.scss";
+import './styles/shrinkwrap.scss';
 
-import React, { ChangeEvent, useCallback, useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
-import { Message } from "./types/sidepanel.types";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {Form, FormLabel, FormSelect } from "react-bootstrap";
-import { getSetting, storeSetting } from "./service/storage";
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Message } from './types/sidepanel.types';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Form, FormLabel, FormSelect } from 'react-bootstrap';
+import { getSetting, storeSetting } from './service/storage';
 
 const SidePanel = () => {
-    const [headline, setHeadline] = useState<string>('boulevard');
-    useEffect(() => {
-        chrome.runtime.onMessage.addListener((msg: Message, sender) => {
-            console.log("message received", msg, sender);
-        });
-    }, []);
+  const [headline, setHeadline] = useState<string>('boulevard');
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener((msg: Message, sender) => {
+      console.log('message received', msg, sender);
+    });
+  }, []);
 
-    useEffect(() => {
-        async function getBrowserSettings() {
-            const headlineSetting = await getSetting("headline");
-            console.log('stored headline: ', headlineSetting);
-            setHeadline(headlineSetting);
-        }
-        getBrowserSettings();
+  useEffect(() => {
+    async function getBrowserSettings() {
+      const headlineSetting = await getSetting('headline');
+      console.log('stored headline: ', headlineSetting);
+      setHeadline(headlineSetting);
+    }
+    getBrowserSettings();
+  }, []);
 
-    }, []);
-
-    const handleSelection = useCallback(async (e: ChangeEvent<HTMLSelectElement>) => {
-        console.log('handleSelection called with: ', e.target.value);
-        if (e.target.value) {
-            storeSetting("headline", e.target.value);
-            setHeadline(e.target.value);
-        }
-    }, []);
-
+  const handleSelection = useCallback(
+    async (e: ChangeEvent<HTMLSelectElement>) => {
+      console.log('handleSelection called with: ', e.target.value);
+      if (e.target.value) {
+        storeSetting('headline', e.target.value);
+        setHeadline(e.target.value);
+      }
+    },
+    [],
+  );
 
   return (
-    <div className={"h-100 d-flex align-items-start flex-column"}>
-        <div className={"row m-2 w-100"}>
-            <div className={"col-1 align-content-center align-content-end"}>
-                <div className="vibrate"></div>
-            </div>
-            <div className={"col-8"}>shrinkwrap.legal ist aktiv.</div>
+    <div className={'h-100 d-flex align-items-start flex-column'}>
+      <div className={'row m-2 w-100'}>
+        <div className={'col-1 align-content-center align-content-end'}>
+          <div className="vibrate"></div>
         </div>
-        <div className={"row mt-3 w-100"}>
-            <p className={"lead"}>
-                Surfen Sie im <a target={"_blank"} href={"https://ris.bka.gv.at/Judikatur/"}>Rechtsinformationssystem des Bundes (RIS)</a>,
-                um KI-generierte Urteilsübersichten direkt im RIS zu erhalten.
-            </p>
-        </div>
-        <div className={"row mt-3 w-100"}>
-            <h3>Einstellungen</h3>
-            <Form>
-                <FormLabel htmlFor={'target'} >Stil der Titel</FormLabel>
-                <FormSelect onChange={handleSelection} id={'target'} aria-label="Zielgruppe" value={headline}>
-                    <option value={'boulevard'}>Boulevard</option>
-                    <option value={'newspaper'}>Tageszeitung</option>
-                    <option value={'journal'}>Rechtszeitschrift</option>
-                </FormSelect>
-            </Form>
-        </div>
-        <div className={"mt-auto "}>
-            <a href={"https://shrinkwrap.legal"} target={"_blank"} className={"text-muted"}>Impressum</a>
-        </div>
-
+        <div className={'col-8'}>shrinkwrap.legal ist aktiv.</div>
+      </div>
+      <div className={'row mt-3 w-100'}>
+        <p className={'lead'}>
+          Surfen Sie im{' '}
+          <a target={'_blank'} href={'https://ris.bka.gv.at/Judikatur/'}>
+            Rechtsinformationssystem des Bundes (RIS)
+          </a>
+          , um KI-generierte Urteilsübersichten direkt im RIS zu erhalten.
+        </p>
+      </div>
+      <div className={'row mt-3 w-100'}>
+        <h3>Einstellungen</h3>
+        <Form>
+          <FormLabel htmlFor={'target'}>Stil der Titel</FormLabel>
+          <FormSelect
+            onChange={handleSelection}
+            id={'target'}
+            aria-label="Zielgruppe"
+            value={headline}
+          >
+            <option value={'boulevard'}>Boulevard</option>
+            <option value={'newspaper'}>Tageszeitung</option>
+            <option value={'journal'}>Rechtszeitschrift</option>
+          </FormSelect>
+        </Form>
+      </div>
+      <div className={'mt-auto '}>
+        <a
+          href={'https://shrinkwrap.legal'}
+          target={'_blank'}
+          className={'text-muted'}
+        >
+          Impressum
+        </a>
+      </div>
     </div>
   );
 };
 
-const root = createRoot(document.getElementById("root")!);
+const root = createRoot(document.getElementById('root')!);
 
 const queryClient = new QueryClient();
 root.render(
--- src/service/storage.ts
+++ src/service/storage.ts
@@ -1,31 +1,28 @@
-
 import browser from "webextension-polyfill";
 
-
-
 function getBrowserApi() {
-    const browserApi = typeof browser !== "undefined" ? browser : chrome;
-    return browserApi;
+  const browserApi = typeof browser !== "undefined" ? browser : chrome;
+  return browserApi;
 }
 
 export function storeSetting(key: string, value: any) {
-    const browserApi = getBrowserApi();
-    console.log('storeSetting', key, value);
-    browserApi.storage.local.set({[key]: value});
+  const browserApi = getBrowserApi();
+  console.log('storeSetting', key, value);
+  browserApi.storage.local.set({[key]: value});
 
 }
 
 export async function getSetting(key: string): Promise<string> {
-    const browserApi = getBrowserApi();
-    const localSetting = await browserApi.storage.local.get(key);
-    console.log('getSetting for',key, localSetting[key]);
+  const browserApi = getBrowserApi();
+  const localSetting = await browserApi.storage.local.get(key);
+  console.log('getSetting for', key, localSetting[key]);
 
-    return localSetting[key] as string;
+  return localSetting[key] as string;
 }
 
 export function getOnChangedListener() {
-    const browserApi = getBrowserApi();
-    return browserApi.storage.onChanged;
+  const browserApi = getBrowserApi();
+  return browserApi.storage.onChanged;
 }
 
 
